7eb8953ce27cbb096aa9350081d8df729581fe2a
Lucas B Budelon <lucasbbudelon@gmail.com>
2018-04-26 20:52:18 -0300

    Changing project names

---
+++ SENACRS.ADS.AlarmClock.Model/Alarm.cs
@@ -0,0 +1,12 @@
+﻿namespace SENACRS.ADS.AlarmClock.Model
+{
+    public class Alarm : Time
+    {
+        public string Message { get; set; }
+
+        public Alarm(decimal hour, decimal minute, decimal second, string message) : base(hour, minute, second)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ SENACRS.ADS.AlarmClock.Model/Clock.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+
+namespace SENACRS.ADS.AlarmClock.Model
+{
+    public class Clock : Time
+    {
+        public int MaxHour { get; }
+        public int MaxMinute { get; }
+        public int MaxSecond { get; }
+
+        public Alarm Alarm { get; set; }
+
+        public Clock(decimal hour, decimal minute, decimal second) : base(hour, minute, second)
+        {
+            MaxHour = 23;
+            MaxMinute = 59;
+            MaxSecond = 59;
+        }
+
+        public void SpendTime()
+        {
+            Thread.Sleep(1000);
+
+            Second++;
+
+            if (Second == MaxSecond)
+            {
+                Second = 0;
+                Minute++;
+            }
+
+            if (Minute == MaxMinute)
+            {
+                Minute = 0;
+                Hour++;
+            }
+
+            if (Hour == MaxHour)
+            {
+                Hour = 0;
+            }
+        }
+
+        public bool IsTimeForAlarm()
+        {
+            return Alarm.Hour == Hour && Alarm.Minute == Minute && Alarm.Second == Second;
+        }
+    }
+}
